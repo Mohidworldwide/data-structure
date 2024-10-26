98c9ce2a34885b4b768b3c35c370cbb9d998956f
Mohidworldwide <mohidssharif@gmail.com>
2024-10-26 20:54:42 +0500

    data structures 1

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+#activity 1
+
+# cricketers=["Baber azam","Virat kohli","Shaheen afridi","Rizwan","Bumrah"]
+# print (len(cricketers))
+
+# cricketers.append("naseem")
+
+# cricketers.remove("Bumrah")
+# cricketers.sort()
+# cricketers=cricketers[1:4]
+# print (cricketers)
+
+#activity 2
+
+dict={"name":"Mohid","age":12}
+dict["address"]="mumbai"
+dict["age"]="4555"
+dict.pop("age")
+dict.clear()
+print(dict)
